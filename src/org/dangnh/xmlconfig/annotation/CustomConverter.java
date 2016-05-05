6993417ed3f8b89f223d7d44cdab8ede95e7cbcd
DangNH <dang.gffg@gmail.com>
2016-05-05 17:25:58 +0700

    Added Retention and Target

--- src/org/dangnh/xmlconfig/annotation/CustomConverter.java
+++ src/org/dangnh/xmlconfig/annotation/CustomConverter.java
@@ -2,9 +2,14 @@ package org.dangnh.xmlconfig.annotation;
 
 import org.dangnh.xmlconfig.Converter.Converter;
 
+import java.lang.annotation.*;
+
 /**
  * Created by DangNH on 5/5/2016.
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
 public @interface CustomConverter {
     Class<? extends Converter> value();
 }
